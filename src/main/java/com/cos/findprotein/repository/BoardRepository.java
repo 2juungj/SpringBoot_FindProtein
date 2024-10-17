4255808aef5be42aac721d0a830fa03d76cf8f53
2juungj <y2k2211@naver.com>
2024-10-18 01:37:28 +0900

    업로드 완료

---
+++ src/main/java/com/cos/findprotein/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.cos.findprotein.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cos.findprotein.model.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+}
