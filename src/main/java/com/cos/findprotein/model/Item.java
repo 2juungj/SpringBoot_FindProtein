62ceb551721d31dbb2474f2d613bc1647f79b4fa
2juungj <y2k2211@naver.com>
2024-10-19 16:43:42 +0900

    Entity 생성

---
+++ src/main/java/com/cos/findprotein/model/Item.java
@@ -0,0 +1,42 @@
+package com.cos.findprotein.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false)
+	private String name;				// 상품명
+	
+	private String info;				// 상품 설명
+	
+	@Enumerated(EnumType.STRING)
+	private Category category; // 카테고리		// PROSUPPS
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;					// 상품 등록자
+	
+	private String image;			// 이미지 링크
+	
+}
---
+++ src/main/java/com/cos/findprotein/model/NaverShopSearchItem.java
@@ -0,0 +1,44 @@
+package com.cos.findprotein.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class NaverShopSearchItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "itemId")
+	private Item item;					// 상품 id
+	
+	@Column(nullable = false)
+	private String name;				// 상품명
+	
+	@Column(nullable = false)
+	private String mallName;		// 판매 사이트명
+	
+	@Column(nullable = false)
+	private String link;					// 주소
+	
+	@Column(nullable = false)
+	private int price;					// 가격
+	
+	private String image;			// 상품 이미지
+	
+}
