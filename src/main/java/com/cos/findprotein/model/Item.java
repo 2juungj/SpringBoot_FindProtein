f4cb082198bd5ca741a62bdc8b718411065161a1
2juungj <y2k2211@naver.com>
2024-10-20 20:47:11 +0900

    연관관계 수정

--- src/main/java/com/cos/findprotein/model/Item.java
+++ src/main/java/com/cos/findprotein/model/Item.java
@@ -1,14 +1,20 @@
 package com.cos.findprotein.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,9 +34,6 @@ public class Item {
 	@Column(nullable = false)
 	private String name; // 상품명
 
-	@Column(nullable = false)
-	private String encName; // URL 인코딩 된 상품명
-
 	private String info; // 상품 설명
 
 	@Enumerated(EnumType.STRING)
@@ -41,11 +44,13 @@ public class Item {
 	private User user; // 상품 등록자
 
 	private String image; // 이미지 링크
+	
+	@OneToMany(mappedBy = "item", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private List<NaverShopSearchItem> naverShopSearchItems = new ArrayList<>();
 
 	@Builder
-	public Item(String name, String encName, String info, Category category, User user, String image) {
+	public Item(String name, String info, Category category, User user, String image) {
 		this.name = name;
-		this.encName = encName;
 		this.info = info;
 		this.category = category;
 		this.user = user;
--- src/main/java/com/cos/findprotein/model/NaverShopSearchItem.java
+++ src/main/java/com/cos/findprotein/model/NaverShopSearchItem.java
@@ -2,6 +2,7 @@ package com.cos.findprotein.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,7 +24,7 @@ public class NaverShopSearchItem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "itemId")
 	private Item item;					// 상품 id
 	
