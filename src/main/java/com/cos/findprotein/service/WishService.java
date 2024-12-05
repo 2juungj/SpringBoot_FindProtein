ed9955cdd5bef5814a265378f99f8d3c4518d5a1
2juungj <y2k2211@naver.com>
2024-12-05 21:51:05 +0900

    메소드 위치 변경 및 예외처리 수정

--- src/main/java/com/cos/findprotein/service/WishService.java
+++ src/main/java/com/cos/findprotein/service/WishService.java
@@ -15,6 +15,7 @@ import com.cos.findprotein.model.User;
 import com.cos.findprotein.model.Wish;
 import com.cos.findprotein.model.WishItem;
 import com.cos.findprotein.repository.ItemRepository;
+import com.cos.findprotein.repository.UserRepository;
 import com.cos.findprotein.repository.WishItemRepository;
 import com.cos.findprotein.repository.WishRepository;
 
@@ -27,6 +28,8 @@ public class WishService {
 	private WishItemRepository wishItemRepository;
 	@Autowired
 	private ItemRepository itemRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 	@Transactional
 	public void 위시리스트에추가(Item newItem, PrincipalDetail principal) {
@@ -40,28 +43,38 @@ public class WishService {
 		Item item = itemRepository.findById(newItem.getId()).orElseThrow(() -> { // itemId로 item를 불러온다.
 			return new IllegalArgumentException("상품 불러오기 실패: 아이디를 찾을 수 없음.");
 		});
-		
-		Optional<WishItem> existingWishItem = wishItemRepository.findByWishIdAndItemId(wish.getId(), item.getId()); // wishId와 itemId로 wishItem을 불러온다.
 
-	    // WishItem이 존재할 경우 추가하지 않는다.
-	    if (existingWishItem.isPresent()) {
-	        System.out.println("이미 위시리스트에 추가된 상품입니다.");
-	        return; // 리턴값을 없애고 메소드를 종료합니다.
-	    }
+		Optional<WishItem> existingWishItem = wishItemRepository.findByWishIdAndItemId(wish.getId(), item.getId()); // wishId와
+																													// itemId로
+																													// wishItem을
+																													// 불러온다.
 
-	    // WishItem이 존재하지 않을 경우 생성
-	    WishItem newWishItem = WishItem.builder().wish(wish).item(item).build();
-	    wishItemRepository.save(newWishItem); // 새로운 WishItem 저장
-	    wish.setCount(wishItemRepository.countByWishId(wish.getId())); // 위시리스트의 상품 수 증가
-	    wishRepository.save(wish); // 위시 리스트 업데이트
+		// WishItem이 존재할 경우 추가하지 않는다.
+		if (existingWishItem.isPresent()) {
+			System.out.println("이미 위시리스트에 추가된 상품입니다.");
+			return; // 리턴값을 없애고 메소드를 종료합니다.
+		}
 
-	    System.out.println("상품이 위시리스트에 추가되었습니다.");
+		// WishItem이 존재하지 않을 경우 생성
+		WishItem newWishItem = WishItem.builder().wish(wish).item(item).build();
+		wishItemRepository.save(newWishItem); // 새로운 WishItem 저장
+		wish.setCount(wishItemRepository.countByWishId(wish.getId())); // 위시리스트의 상품 수 증가
+		wishRepository.save(wish); // 위시 리스트 업데이트
+
+		System.out.println("상품이 위시리스트에 추가되었습니다.");
 	}
 
-
 	// 위시 상품 리스트를 모두 불러와 해당 사용자의 wishId와 일치하는 상품만 반환
-	@Transactional(readOnly = true)
-	public List<WishItem> 위시리스트불러오기(Wish wish) {
+	@Transactional
+	public List<WishItem> 위시리스트불러오기(PrincipalDetail principal) {
+
+		User user = userRepository.findById(principal.getUser().getId()).orElseThrow(() -> { // 로그인 한 사용자의 id로 user 데이터 호출
+			return new IllegalArgumentException("유저 데이터 불러오기 실패: 아이디를 찾을 수 없음.");
+		});
+		Wish wish = wishRepository.findByUserId(user.getId()).orElseGet(() -> { // 로그인 한 사용자의 위시리스트 호출, 위시리스트가 없다면 생성
+			Wish newWish = Wish.builder().user(user).count(0).build();
+			return wishRepository.save(newWish);
+		});
 
 		int userWishId = wish.getId(); // 사용자의 wishId 불러오기
 
@@ -74,13 +87,13 @@ public class WishService {
 				userWishItemList.add(wishItem);
 			}
 		}
-		
-		 // 리스트를 역순으로 정렬 (최신 상품이 상단으로)
-        Collections.reverse(userWishItemList);
-        
+
+		// 리스트를 역순으로 정렬 (최신 상품이 상단으로)
+		Collections.reverse(userWishItemList);
+
 		return userWishItemList;
 	}
-	
+
 	@Transactional
 	public void 위시상품삭제(int id, PrincipalDetail principal) {
 		User user = principal.getUser();
@@ -88,13 +101,13 @@ public class WishService {
 			return new IllegalArgumentException("위시리스트 불러오기 실패: 아이디를 찾을 수 없음.");
 		});
 		int wishCount = wishItemRepository.countByWishId(wish.getId()); // 해당 유저의 wishItem의 수량 불러오기
-		
+
 		WishItem wishItem = wishItemRepository.findById(id).orElseThrow(() -> { // 해당 위시 상품이 DB에 있는지 확인
 			return new IllegalArgumentException("위시리스트 상품 불러오기 실패: 아이디를 찾을 수 없음.");
 		});
 		wishItemRepository.deleteById(id); // wishItemId로 해당 위시 상품 삭제
 		wishCount -= 1; // 위시 상품이 삭제되었으므로 수량 -1
-		
+
 		wish.setCount(wishCount); // 수정된 count를 wish에 저장
 		wishRepository.save(wish); // 수정된 wish를 DB에 저장
 	}
