58aac71fee6f463a57e7bc2a417b04d01507a43a
2juungj <y2k2211@naver.com>
2024-12-05 21:52:30 +0900

    알림 발생 시각 추가

--- src/main/java/com/cos/findprotein/model/Notifications.java
+++ src/main/java/com/cos/findprotein/model/Notifications.java
@@ -1,5 +1,7 @@
 package com.cos.findprotein.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,12 +34,16 @@ public class Notifications {
 	private String content; // 알림 내용
 	
 	private String link; // 최저가 비교 페이지 링크
+	
+	@Temporal(TemporalType.TIMESTAMP)  // 필드가 Date 타입일 경우 추가
+    private Date createTime;  // 알림이 발생 된 시간 // LocalDateTime 대신 Date 사용
 
 	@Builder
-	public Notifications(Notification notification, String content, String link) {
+	public Notifications(Notification notification, String content, String link, Date createTime) {
 		this.notification = notification;
 		this.content = content;
 		this.link = link;
+		this.createTime = createTime;
 	}
 
 }
